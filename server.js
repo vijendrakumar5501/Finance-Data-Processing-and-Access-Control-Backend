c810bdbd3df722c46da84324cf68bdd880e00337
vijendra <vijendrakumar5501@gmail.com>
2026-04-02 12:30:07 +0530

    added app

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
